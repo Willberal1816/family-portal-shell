721e480f9e1a3abef2fcfbe71228c736d3e6987f
Willberal1816 <rmoran4446@mac.com>
2026-09-08 21:46:01 -0500

    Add curriculum portal-controls compatibility shim

---
+++ worksheets/curriculum/school-page-controls.js
@@ -0,0 +1,9 @@
+(function(){
+  'use strict';
+  if(window.__moranSchoolControlsCompatLoading)return;
+  window.__moranSchoolControlsCompatLoading=true;
+  var s=document.createElement('script');
+  s.src='/family-portal-shell/worksheets/school-page-controls.js?v=20260908-compat';
+  s.defer=true;
+  document.head.appendChild(s);
+})();
